e63e501bd76d0748cdd235a98cc382125db748bb
Iuri Andreazza <iuri.andreazza@gmail.com>
2017-04-02 19:22:44 -0300

    fixing all stuff

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -1,16 +1,23 @@
-package com.herokufelipe.controller;
+package com.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
+/**
+ * Created by iuriandreazza on 02/04/17.
+ */
+@Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public ModelAndView home() {
+    @RequestMapping("/welcome")
+    public ModelAndView helloWorld() {
+
         String message = "<br><div style='text-align:center;'>"
                 + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
         return new ModelAndView("welcome", "message", message);
     }
+
 
 }
